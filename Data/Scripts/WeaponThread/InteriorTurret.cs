02478b24d2398108c093f7158781ae83ef59ac5a
Bryon <30876973+nukeguard@users.noreply.github.com>
2020-04-23 14:33:16 -0400

    particle change

--- Data/Scripts/WeaponThread/InteriorTurret.cs
+++ Data/Scripts/WeaponThread/InteriorTurret.cs
@@ -140,7 +140,7 @@ namespace WeaponThread
                     {
                         Name = "Muzzle_Flash_Large",//Muzzle_Flash_Large
                         Color = Color(red: 1, green: 1, blue: 1, alpha: 1),
-                        Offset = Vector(x: 0, y: -1, z: 0),
+                        Offset = Vector(x: 0, y: 0, z: 0),
                         Extras = new ParticleOptionDef
                         {
                             Loop = false,

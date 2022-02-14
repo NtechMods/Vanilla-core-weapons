93d21602cb6c9f51e0824e9faf4b9317438a1a31
Bryon <bryonjs222@gmail.com>
2022-02-13 20:03:04 -0500

    Update Railguns.cs

--- Data/Scripts/CoreParts/Railguns.cs
+++ Data/Scripts/CoreParts/Railguns.cs
@@ -135,7 +135,7 @@ namespace Scripts {
                     BarrelSpinRate = 0, // Visual only, 0 disables and uses RateOfFire.
                     DeterministicSpin = false, // Spin barrel position will always be relative to initial / starting positions (spin will not be as smooth).
                     SpinFree = false, // Spin barrel while not firing.
-                    StayCharged = false, // Will start recharging whenever power cap is not full.
+                    StayCharged = true, // Will start recharging whenever power cap is not full.
                 },
                 Audio = new HardPointAudioDef
                 {
@@ -154,7 +154,7 @@ namespace Scripts {
                     Effect1 = new ParticleDef
                     {
                         Name = "", // SubtypeId of muzzle particle effect.
-                        Color = Color(red: 0, green: 0, blue: 0, alpha: 1), // Deprecated, set color in particle sbc.
+                        Color = Color(red: 1, green: 1, blue: 1, alpha: 1), // Deprecated, set color in particle sbc.
                         Offset = Vector(x: 0, y: 0, z: 0), // Offsets the effect from the muzzle empty.
                         Extras = new ParticleOptionDef
                         {

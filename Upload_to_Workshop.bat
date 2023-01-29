396aaf08b8632f7a1d7eb476d1015e0aa147d27c
Bryon <bryonjs222@gmail.com>
2023-01-29 13:58:18 -0500

    Update Upload_to_Workshop.bat

--- Upload_to_Workshop.bat
+++ Upload_to_Workshop.bat
@@ -1,4 +1,4 @@
-set SEInstallDir="C:\Steam\steamapps\common\SpaceEngineers"
+set SEInstallDir="C:\steam\steamapps\common\SpaceEngineers"
 for %%I in (.) do set ParentDirName=%%~nxI
-%SEInstallDir%\Bin64\SEWorkshopTool.exe push --mods "%ParentDirName%" --exclude-ext .bat .gif .psd .fbx .hkt .xml .blend .blend1 .mp4
+%SEInstallDir%\Bin64\SEWorkshopTool.exe push --mods "%ParentDirName%" --exclude-ext .bat .psd .fbx .hkt .xml .blend .blend1
 pause
